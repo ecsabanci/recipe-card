17544a1bb92daea58ddbf0c48ed91a8d9da76c58
Emre Sabancı <ecsabancii@gmail.com>
2021-06-15 16:39:01 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import "./App.css";
+import Card from "./components/Card/Card";
+import food from "./assets/food.jpg"; // Tell webpack this JS file uses this image
+import {useState} from 'react';
+
+function App() {
+  const recipeAuthor = "Kodluyoruz";
+  const recipeItem = {
+    title: "Avokado Ezmeli Taco",
+    date: "12 Ocak 2021, Salı",
+    image: food,
+    description:
+      "Bu kremsi ve baharatlı avokado sosu, günlük taco'larınızı hazırlamak için harika seçeneklerden biri. Geleneksel olarak flautas veya taquitos ile servis edilir, ancak bazı vegan enchiladalara da harika bir katkı sağlar.",
+  };
+
+  const likeCount = 193;
+  const [isLiked, setIsLiked] = useState(false);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Card
+          /* prop ismi = { değişken } */
+          item={recipeItem}
+          author={recipeAuthor}
+          likeCount={likeCount}
+          isLiked={isLiked}
+          foodImg={food}
+          setIsLiked={setIsLiked}
+        />
+      </header>
+    </div>
+  );
+}
+
+export default App;
