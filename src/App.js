d13d12c13600ff4ec3a3d6a5f5a7c2dc73eddfd8
Emre Sabancı <ecsabancii@gmail.com>
2021-06-16 16:02:54 +0300

    api like structure and responsive design

--- src/App.js
+++ src/App.js
@@ -1,35 +1,93 @@
 import "./App.css";
 import Card from "./components/Card/Card";
 import food from "./assets/food.jpg"; // Tell webpack this JS file uses this image
-import {useState} from 'react';
+// import {useState} from 'react';
 
 function App() {
-  const recipeAuthor = "Kodluyoruz";
-  const recipeItem = {
-    title: "Avokado Ezmeli Taco",
-    date: "12 Ocak 2021, Salı",
-    image: food,
-    description:
-      "Bu kremsi ve baharatlı avokado sosu, günlük taco'larınızı hazırlamak için harika seçeneklerden biri. Geleneksel olarak flautas veya taquitos ile servis edilir, ancak bazı vegan enchiladalara da harika bir katkı sağlar.",
-  };
 
-  
-  const [likeCount, setLikeCount] = useState(193);
-  const [isLiked, setIsLiked] = useState(false);
+  // API like structure
+  const recipeItem = [
+    {
+      id: 0,
+      recipeAuthor: "Kodluyoruz",
+      title: "Avokado Ezmeli Taco",
+      date: "12 Ocak 2021, Salı",
+      likeNum: 20,
+      image: food,
+      description:
+        "Bu kremsi ve baharatlı avokado sosu, günlük taco'larınızı hazırlamak için harika seçeneklerden biri. Geleneksel olarak flautas veya taquitos ile servis edilir, ancak bazı vegan enchiladalara da harika bir katkı sağlar.",
+    },
+    {
+      id: 1,
+      recipeAuthor: "Emre",
+      title: "A Food",
+      date: "13 Ocak 2021, Çarşamba",
+      likeNum: 22,
+      image: food,
+      description:
+        "Bu kremsi ve baharatlı avokado sosu, günlük taco'larınızı hazırlamak için harika seçeneklerden biri. Geleneksel olarak flautas veya taquitos ile servis edilir, ancak bazı vegan enchiladalara da harika bir katkı sağlar.",
+    },
+    {
+      id: 2,
+      recipeAuthor: "Cagri",
+      title: "B Food",
+      date: "14 Ocak 2021, Perşembe",
+      likeNum: 23,
+      image: food,
+      description:
+        "Bu kremsi ve baharatlı avokado sosu, günlük taco'larınızı hazırlamak için harika seçeneklerden biri. Geleneksel olarak flautas veya taquitos ile servis edilir, ancak bazı vegan enchiladalara da harika bir katkı sağlar.",
+    },
+    {
+      id: 3,
+      recipeAuthor: "Abdo",
+      title: "C Food",
+      date: "15 Ocak 2021, Cuma",
+      likeNum: 30,
+      image: food,
+      description:
+        "Bu kremsi ve baharatlı avokado sosu, günlük taco'larınızı hazırlamak için harika seçeneklerden biri. Geleneksel olarak flautas veya taquitos ile servis edilir, ancak bazı vegan enchiladalara da harika bir katkı sağlar.",
+    },
+    {
+      id: 4,
+      recipeAuthor: "Faruk",
+      title: "D Food",
+      date: "16 Ocak 2021, Cumartesi",
+      likeNum: 35,
+      image: food,
+      description:
+        "Bu kremsi ve baharatlı avokado sosu, günlük taco'larınızı hazırlamak için harika seçeneklerden biri. Geleneksel olarak flautas veya taquitos ile servis edilir, ancak bazı vegan enchiladalara da harika bir katkı sağlar.",
+    },
+    {
+      id: 4,
+      recipeAuthor: "Mert",
+      title: "E Food",
+      date: "17 Ocak 2021, Pazar",
+      likeNum: 40,
+      image: food,
+      description:
+        "Bu kremsi ve baharatlı avokado sosu, günlük taco'larınızı hazırlamak için harika seçeneklerden biri. Geleneksel olarak flautas veya taquitos ile servis edilir, ancak bazı vegan enchiladalara da harika bir katkı sağlar.",
+    },
+    
+
+  ]
+
 
   return (
     <div className="App">
       <header className="App-header">
-        <Card
-          /* prop ismi = { değişken } */
-          item={recipeItem}
-          author={recipeAuthor}
-          likeCount={likeCount}
-          isLiked={isLiked}
-          foodImg={food}
-          setIsLiked={setIsLiked}
-          setLikeCount={setLikeCount}
-        />
+        <div className="recipies">
+          {recipeItem.map((data,index) => (
+            <Card
+            /* prop ismi = { değişken } */
+              item={data}
+              author={data.recipeAuthor}
+              foodImg={data.image}
+              likeNum={data.likeNum}
+              key={index}
+            />
+          ))}
+        </div>
+        
       </header>
     </div>
   );
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,23 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Card.css";
 
 import heartOutline from "../../assets/heart-outline.png"; // Tell webpack this JS file uses this image
 import heartFill from "../../assets/heart-fill.png"; // Tell webpack this JS file uses this image
 
-export default function Card({item,author,likeCount,isLiked,foodImg,setIsLiked,setLikeCount}) {
+export default function Card({item,author,foodImg,likeNum}) {
+
+  const [likeCount, setLikeCount] = useState(likeNum);
+  const [isLiked, setIsLiked] = useState(false);
+
+  
+
   return (
     <div className="card">
-      <div className="card-header">
-        <div className="profile">
-          <span className="letter">{author[0]}</span>
+      <div className="main">
+        <div className="card-header">
+          <div className="profile">
+            <span className="letter">{author[0]}</span>
+          </div>
+          <div className="card-title-group">
+            <h5 className="card-title">{item.title}</h5>
+            <div className="card-date">{item.date}</div>
+          </div>
         </div>
-        <div className="card-title-group">
-          <h5 className="card-title">{item.title}</h5>
-          <div className="card-date">{item.date}</div>
+        <img className="card-image" src={foodImg} alt="Logo" />
+        <div className="card-text">{item.description}</div>
         </div>
-      </div>
-      <img className="card-image" src={foodImg} alt="Logo" />
-      <div className="card-text">{item.description}</div>
       <div className="card-like-bar"  onClick={() => [setIsLiked(!isLiked), !isLiked ? setLikeCount(likeCount+1) : setLikeCount(likeCount-1)]} >
         {isLiked ? (
           <img className="card-like-icon filled" src={heartFill} alt="Logo" />
